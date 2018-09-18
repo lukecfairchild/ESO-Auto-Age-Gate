6d8069d02c538fb5cfa50e96ce876e6ba4a6fd2d
Luke Fairchild <lukecfairchild@gmail.com>
2018-09-18 13:16:03 -0600

    Update to work with angular

--- main.user.js
+++ main.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         ESO Auto Age Gate
-// @version      0.2
+// @version      0.3
 // @description  Auto Enters a age on ESO's Age Gate
 // @author       Luke Fairchild
-// @match        https://www.elderscrollsonline.com/en-us/agegate
+// @match        https://www.elderscrollsonline.com/en-us/agegate?*
 // ==/UserScript==
 
 (function() {
-    'use strict';
-
-  $('#month').val(1);
-  $('#day').val(1);
-  $('#year').val(1950);
-  $('form')[0].submit();
+	'use strict';
+return
+	$('#day').val('1').change();
+	$('#month').val('1').change();
+	$('#year').val('1950').change();
+	$('.btn.btn-gate')[0].click();
 })();
